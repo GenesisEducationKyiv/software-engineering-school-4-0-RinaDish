9912fd13baad56273c14cd10c7f7377527f717e0
Iryna Dishli <dishli@MacBook-Pro-Iryna.local>
2024-06-14 23:16:12 +0300

    hw2 refactor: moved connect to db to app module

--- currency-rates/internal/app/app.go
+++ currency-rates/internal/app/app.go
@@ -11,6 +11,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-co-op/gocron/v2"
 	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type App struct {
@@ -30,10 +32,8 @@ func (app *App) Run(ctx context.Context) error {
 	privatClient := clients.NewPrivatClient(app.l)
 	rateService := services.NewRate(app.l, []services.RateClient{nbuClient, privatClient})
 
-	adminRepository, err := repo.NewAdminRepository(app.cfg.DBUrl, app.l)
-	if err != nil {
-		return err
-	}
+	db, _ := gorm.Open(postgres.Open(app.cfg.DBUrl), &gorm.Config{}) //err
+	adminRepository := repo.NewAdminRepository(db, app.l)
 
 	subscriptionSender := services.NewEmail(app.cfg.EmailAddress, app.cfg.EmailPass, app.l)
 	subscriptionService := services.NewSubscriptionService(app.l, adminRepository, subscriptionSender, rateService)

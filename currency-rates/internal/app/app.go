1bc0e36a58f84cad7c677cd00b7b2b1e410f5359
Iryna Dishli <dishli@MacBook-Pro-Iryna.local>
2024-06-14 23:21:35 +0300

    hw2 refactor: added error handling

--- currency-rates/internal/app/app.go
+++ currency-rates/internal/app/app.go
@@ -32,7 +32,11 @@ func (app *App) Run(ctx context.Context) error {
 	privatClient := clients.NewPrivatClient(app.l)
 	rateService := services.NewRate(app.l, []services.RateClient{nbuClient, privatClient})
 
-	db, _ := gorm.Open(postgres.Open(app.cfg.DBUrl), &gorm.Config{}) //err
+	db, err := gorm.Open(postgres.Open(app.cfg.DBUrl), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	
 	adminRepository := repo.NewAdminRepository(db, app.l)
 
 	subscriptionSender := services.NewEmail(app.cfg.EmailAddress, app.cfg.EmailPass, app.l)
